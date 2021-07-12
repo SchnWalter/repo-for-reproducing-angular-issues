e16224e19d49943e068d4101fab87473f50ad89b
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2021-07-12 17:48:35 +0300

    Use @example/sdk in the application.

--- example-application/web/src/app/app.component.ts
+++ example-application/web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VERSION } from '@example/sdk';
 
 @Component({
   selector: 'web-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web';
+  version = VERSION
 }
